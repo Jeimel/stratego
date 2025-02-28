405eb8d8aa4ea3a83ee060312df53fcc4286d38a
Felix Jablinski <jablinskifelix@gmail.com>
2025-02-28 11:57:11 +0100

    `MoveList` to store generated moves

--- src/position.rs
+++ src/position.rs
@@ -13,3 +13,37 @@ impl Move {
         piece: 0,
     };
 }
+
+pub struct MoveList {
+    pub length: usize,
+    moves: [Move; 218],
+}
+
+impl Default for MoveList {
+    fn default() -> Self {
+        MoveList {
+            moves: [Move::NULL; 218],
+            length: 0,
+        }
+    }
+}
+
+impl std::ops::Index<usize> for MoveList {
+    type Output = Move;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.moves[index]
+    }
+}
+
+impl MoveList {
+    pub fn push(&mut self, from: u8, to: u8, flag: u8, piece: u8) {
+        self.moves[self.length] = Move {
+            from,
+            to,
+            flag,
+            piece,
+        };
+        self.length += 1;
+    }
+}
