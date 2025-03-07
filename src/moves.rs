ce46594d49dd59ac553a4852daa8404e5b8b1c55
Felix Jablinski <jablinskifelix@gmail.com>
2025-03-07 17:03:29 +0100

    `MoveStack` to handle repetitions

--- src/moves.rs
+++ src/moves.rs
@@ -56,3 +56,60 @@ impl MoveList {
         self.length += 1;
     }
 }
+
+#[derive(Default)]
+pub struct MoveStack {
+    stack: Vec<u64>,
+    ply: u16,
+}
+
+impl MoveStack {
+    pub fn print(&self) {
+        for &hash in self.stack.iter().rev() {
+            println!("Hash {hash}");
+        }
+    }
+
+    pub fn repetition(&self, half: usize, current: u64) -> bool {
+        if self.stack.len() < 8 {
+            return false;
+        }
+
+        for &hash in self.stack.iter().rev().take(half + 1).skip(1).step_by(2) {
+            if hash == current {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    pub fn push(&mut self, hash: u64) {
+        self.ply += 1;
+        self.stack.push(hash);
+    }
+
+    pub fn pop(&mut self) {
+        self.ply -= 1;
+        self.stack.pop();
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct SquareMask {
+    pub moves: u8,
+    pub from: u8,
+    pub to: u8,
+    pub path: u64,
+}
+
+impl Default for SquareMask {
+    fn default() -> Self {
+        SquareMask {
+            moves: 0,
+            from: u8::MAX,
+            to: u8::MAX,
+            path: 0,
+        }
+    }
+}
