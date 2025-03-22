7e1a2166a40d3dda10323f68476a9cf2e25509eb
Felix Jablinski <jablinskifelix@gmail.com>
2025-03-22 16:41:01 +0100

    Replace `Protocol` with `Algorithm` enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,128 +1,42 @@
-use std::time::Instant;
-
-use rand::seq::IndexedRandom;
-use stratego::{Move, MoveStack, Position};
-use tournament::DEPLOYMENTS;
+use mcts::{ISMCTS, MCTS};
+use rand::{rng, seq::IndexedRandom};
+use random::UniformRandom;
+use stratego::{Move, StrategoState};
 
 pub mod mcts;
+pub mod random;
 pub mod stratego;
 pub mod tournament;
 
-const EMPTY_POS: &str = "8/8/8/8/8/8/8/8 r";
+// Deployments from pov of blue
+pub const DEPLOYMENTS: [&str; 2] = ["5c2/2csdbd1/3gbfm1", "5cbf/5cdb/1gsd3m"];
 
-pub trait Protocol: Default {
-    fn run(&mut self) {
-        Self::identify();
+pub enum Algorithm {
+    MCTS(MCTS),
+    ISMCTS(ISMCTS),
+    Random(UniformRandom),
+}
 
-        let mut pos = Position::from(EMPTY_POS);
-        let mut stack = MoveStack::default();
-
-        loop {
-            let mut input = String::new();
-            let bytes = std::io::stdin().read_line(&mut input).unwrap();
-
-            if bytes == 0 {
-                break;
-            }
-
-            let commands = input.split_whitespace().collect::<Vec<_>>();
-            let command = *commands.first().unwrap();
-
-            match command {
-                "quit" => std::process::exit(0),
-                "setoption" => self.handle_options(commands),
-                "position" => self.set_position(commands, &mut pos, &mut stack),
-                "newgame" => {
-                    pos = Position::from(EMPTY_POS);
-                    stack = MoveStack::default();
-                }
-                "isready" => println!("readyok"),
-                "deployment" => println!("deployment {}", self.handle_deployment(commands)),
-                "go" => println!("bestmove {}", self.handle_go(commands, &pos, &stack)),
-                "perft" => run_perft(&pos, &mut stack.clone(), 5),
-                "d" => println!("{pos}"),
-                _ => println!("Unknown command: {command}"),
-            }
+impl Algorithm {
+    pub fn go(&mut self, pos: &StrategoState) -> Move {
+        match self {
+            Algorithm::MCTS(a) => a.go(pos),
+            Algorithm::ISMCTS(a) => a.go(pos),
+            Algorithm::Random(a) => a.go(pos),
         }
     }
 
-    fn identify() {
-        println!("{}", preamble());
-        println!("{}\nok", Self::option());
-    }
-
-    fn set_position(&mut self, commands: Vec<&str>, pos: &mut Position, stack: &mut MoveStack) {
-        let mut notation = String::from(commands[1]);
-        notation.push(' ');
-        notation.push_str(commands[2]);
-
-        *pos = Position::from(&notation);
-        stack.push(pos.hash());
-
-        for mov in commands.iter().skip(4) {
-            if let Some(mov) = pos.gen(stack).iter().find(|m| *mov == format!("{m}")) {
-                pos.make(&mov);
-                stack.push(pos.hash());
-            }
+    pub fn deployment(&mut self) -> String {
+        match self {
+            Algorithm::MCTS(_) => random_deployment(),
+            Algorithm::ISMCTS(_) => random_deployment(),
+            Algorithm::Random(a) => a.deployment(),
         }
     }
-
-    /// Must return a valid deployment from pov of blue in notation
-    fn handle_deployment(&self, _: Vec<&str>) -> String {
-        let mut rng = rand::rng();
-
-        DEPLOYMENTS.choose(&mut rng).unwrap().to_string()
-    }
-
-    /// Must return list of valid options in the format:
-    /// setoption name <name> = <value>
-    fn option() -> String;
-
-    /// Must return a valid move in the given position
-    fn handle_go(&mut self, commands: Vec<&str>, pos: &Position, stack: &MoveStack) -> Move;
-
-    /// Set options for the given algorithm
-    fn handle_options(&mut self, commands: Vec<&str>);
 }
 
-fn preamble() -> String {
-    String::from(concat!(
-        "id name stratego",
-        '\n',
-        "id author Felix Jablinski"
-    ))
-}
+fn random_deployment() -> String {
+    let mut rng = rng();
 
-fn run_perft(pos: &Position, stack: &mut MoveStack, depth: usize) {
-    let now = Instant::now();
-    let nodes = perft(pos, stack, depth);
-
-    let time = now.elapsed().as_micros();
-    println!(
-        "perft {depth} time {} nodes {nodes} ({:.2} Mnps)",
-        time / 1000,
-        nodes as f32 / time as f32
-    );
-}
-
-fn perft(pos: &Position, stack: &mut MoveStack, depth: usize) -> usize {
-    if depth == 0 {
-        return 1;
-    }
-
-    let mut nodes = 0;
-
-    let moves = pos.gen(stack);
-    for i in 0..moves.length() {
-        let mut new_pos = *pos;
-
-        new_pos.make(&moves[i]);
-        stack.push(new_pos.hash());
-        let child_nodes = perft(&new_pos, stack, depth - 1);
-        stack.pop();
-
-        nodes += child_nodes;
-    }
-
-    nodes
+    DEPLOYMENTS.choose(&mut rng).unwrap().to_string()
 }
