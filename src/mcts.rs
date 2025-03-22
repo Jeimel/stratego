8c5e46679f169ee195cc0d1113e5f5dbae4de288
Felix Jablinski <jablinskifelix@gmail.com>
2025-03-22 16:44:25 +0100

    Remove `Protocol´ for MCTS

--- src/mcts.rs
+++ src/mcts.rs
@@ -1,38 +1,7 @@
-use crate::stratego::{GameState, MoveStack, Position};
-use rand::seq::IteratorRandom;
-
+mod ismcts;
 mod iteration;
+mod mcts;
 mod node;
-mod search;
 
-pub use search::Search;
-
-impl Position {
-    pub fn rollout(&mut self, stack: &mut MoveStack) -> f32 {
-        let mut rng = rand::rng();
-
-        let stm = self.stm();
-
-        let mut moves = Vec::new();
-        while !self.game_over() {
-            let mov = self.gen(&stack).iter().choose(&mut rng);
-
-            if let Some(m) = mov {
-                moves.push(m);
-                self.make(&m);
-                stack.push(self.hash());
-            } else {
-                // Handle two-squares and more-squares rule
-                self.set_game_state(GameState::Loss);
-            }
-        }
-
-        let current = f32::from(stm == self.stm());
-        match self.game_state() {
-            GameState::Draw => 0.0,
-            GameState::Win => 1.0 + (-2.0 * current),
-            GameState::Loss => -1.0 + (2.0 * current),
-            GameState::Ongoing => unreachable!(),
-        }
-    }
-}
+pub use ismcts::ISMCTS;
+pub use mcts::MCTS;
