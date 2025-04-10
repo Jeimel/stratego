7148838ae7a5f5635db100ddd0314f22edae4d0c
Felix Jablinski <jablinskifelix@gmail.com>
2025-04-10 18:29:20 +0200

    PIMC

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use mcts::{ISMCTS, MCTS};
+use mcts::{ISMCTS, MCTS, PIMC};
 use rand::{rng, seq::IndexedRandom};
 use random::UniformRandom;
 use stratego::{Move, StrategoState};
@@ -13,6 +13,7 @@ pub const DEPLOYMENTS: [&str; 2] = ["5c2/2csdbd1/3gbfm1", "5cbf/5cdb/1gsd3m"];
 
 pub enum Algorithm {
     MCTS(MCTS),
+    PIMC(PIMC),
     ISMCTS(ISMCTS),
     Random(UniformRandom),
 }
@@ -21,6 +22,7 @@ impl Algorithm {
     pub fn go(&mut self, pos: &StrategoState) -> Move {
         match self {
             Algorithm::MCTS(a) => a.go(pos),
+            Algorithm::PIMC(a) => a.go(pos),
             Algorithm::ISMCTS(a) => a.go(pos),
             Algorithm::Random(a) => a.go(pos),
         }
@@ -29,6 +31,7 @@ impl Algorithm {
     pub fn deployment(&mut self) -> String {
         match self {
             Algorithm::MCTS(_) => random_deployment(),
+            Algorithm::PIMC(_) => random_deployment(),
             Algorithm::ISMCTS(_) => random_deployment(),
             Algorithm::Random(a) => a.deployment(),
         }
---
+++ src/mcts/pimc.rs
@@ -0,0 +1,51 @@
+use super::{node::NodeStats, MCTS, UCT};
+use crate::stratego::{Move, StrategoState};
+use std::collections::HashMap;
+
+pub struct PIMC {
+    determinizations: usize,
+    iterations: usize,
+}
+
+impl UCT for PIMC {}
+
+impl PIMC {
+    pub fn new(determinizations: usize, iterations: usize) -> Self {
+        Self {
+            determinizations,
+            iterations,
+        }
+    }
+
+    pub fn go(&mut self, pos: &StrategoState) -> Move {
+        let mut root: HashMap<Move, NodeStats> = HashMap::new();
+
+        for _ in 0..self.determinizations {
+            let mut search = MCTS::new(self.iterations);
+            let mut det = pos.determination();
+
+            search.run(&mut det);
+
+            search.root().children().for_each(|c| {
+                let stats = c.stats();
+                let entry = root.entry(c.mov().unwrap()).or_insert(NodeStats::default());
+
+                entry.visits += stats.visits;
+                entry.reward += stats.reward;
+            })
+        }
+
+        let mut children: Vec<_> = root.iter().collect();
+        children.sort_by_key(|c| c.1.visits);
+        for c in &children {
+            let stats = c.1;
+
+            println!(
+                "info move {} visits {} reward {}",
+                c.0, stats.visits, stats.reward,
+            );
+        }
+
+        *children.iter().max_by_key(|c| c.1.visits).unwrap().0
+    }
+}
