f2ca0939e76489d798616be7beeec2d6f4125dd9
Felix Jablinski <jablinskifelix@gmail.com>
2025-04-24 16:48:26 +0200

    Improve `ReplayBuffer` I/O

--- training/src/buffer.rs
+++ training/src/buffer.rs
@@ -1,39 +1,71 @@
-use bincode::{config, Decode, Encode};
+use bincode::{
+    config::{self},
+    Decode, Encode,
+};
 use std::{
-    fs::File,
-    io::Write,
+    collections::VecDeque,
+    fs::{File, OpenOptions},
+    io::BufReader,
     sync::atomic::{AtomicBool, Ordering},
 };
-use stratego::stratego::Move;
+use stratego::stratego::{Move, StrategoState};
 
-#[derive(Encode, Decode)]
-pub struct MoveData {
-    pub mov: Move,
-    pub score: f32,
+#[derive(Clone, Encode, Decode)]
+pub struct SearchData {
+    pub input: [f32; StrategoState::FEATURES],
+    pub target: f32,
     pub policy: Vec<(Move, usize)>,
 }
 
-#[derive(Encode, Decode)]
-pub struct SearchData {
-    pub pos: String,
-    pub result: f32,
-    pub moves: Vec<MoveData>,
-}
-
 impl SearchData {
-    pub fn push(&mut self, mov: Move, score: f32, policy: Vec<(Move, usize)>) {
-        self.moves.push(MoveData { mov, score, policy });
+    pub fn new(input: [f32; StrategoState::FEATURES], policy: Vec<(Move, usize)>) -> Self {
+        Self {
+            input,
+            target: 0.0,
+            policy,
+        }
     }
 }
 
 pub struct ReplayBuffer {
     pub file: File,
+    pub dataset: VecDeque<SearchData>,
+    pub size: usize,
     pub games: usize,
     pub limit: usize,
 }
 
 impl ReplayBuffer {
-    pub fn push(&mut self, data: &SearchData, abort: &AtomicBool) {
+    pub fn new(path: &str, size: usize, limit: usize) -> Self {
+        let file = OpenOptions::new()
+            .append(true)
+            .create(true)
+            .open(path)
+            .unwrap();
+
+        let mut buffer = ReplayBuffer {
+            file,
+            dataset: VecDeque::with_capacity(size),
+            size,
+            games: 0,
+            limit,
+        };
+
+        let config = config::standard();
+
+        let file = File::open(path).unwrap();
+        let mut reader = BufReader::new(file);
+        loop {
+            match bincode::decode_from_reader(&mut reader, config) {
+                Ok(search_data) => buffer.push_pop(search_data),
+                Err(_) => break,
+            }
+        }
+
+        buffer
+    }
+
+    pub fn push(&mut self, data: Vec<SearchData>, abort: &AtomicBool) {
         if abort.load(Ordering::Relaxed) {
             return;
         }
@@ -41,21 +73,38 @@ impl ReplayBuffer {
         self.games += 1;
 
         let config = config::standard();
-        let encoded: Vec<u8> = bincode::encode_to_vec(data, config).unwrap();
+        for search_data in data {
+            bincode::encode_into_std_write(&search_data, &mut self.file, config).unwrap();
 
-        Write::write_all(&mut self.file, &encoded).unwrap();
+            self.push_pop(search_data);
+        }
 
         if self.games >= self.limit {
             abort.store(true, Ordering::Relaxed);
             return;
         }
 
-        if self.games % 32 == 0 {
+        if self.games % 128 == 0 {
             self.report();
         }
     }
 
+    pub fn reset(&mut self) {
+        self.games = 0;
+    }
+
     pub fn report(&self) {
-        println!("info datagen games {}", self.games);
+        println!(
+            "info datagen games {} dataset {}",
+            self.games,
+            self.dataset.len()
+        );
+    }
+
+    fn push_pop(&mut self, data: SearchData) {
+        if self.dataset.len() == self.size {
+            self.dataset.pop_front();
+        }
+        self.dataset.push_back(data);
     }
 }
