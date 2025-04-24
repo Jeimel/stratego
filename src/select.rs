32ba94d3f1def097c463ee2357b464d8fd6827fb
Felix Jablinski <jablinskifelix@gmail.com>
2025-04-24 16:46:35 +0200

    Custom parameter for `Select`

--- src/select.rs
+++ src/select.rs
@@ -1,53 +1,49 @@
 use crate::mcts::Node;
 
 pub enum Select {
-    UCT,
-    ISUCT,
-    ProgressiveUCT,
-    ProgressiveISUCT,
+    UCT(f32),
+    ISUCT(f32),
+    ProgressiveUCT(f32, f32),
+    ProgressiveISUCT(f32, f32),
 }
 
 impl Select {
     pub fn get(&self, node: &Node) -> f32 {
         match self {
-            Select::UCT => uct(node),
-            Select::ISUCT => isuct(node),
-            Select::ProgressiveUCT => progressive_uct(node),
-            Select::ProgressiveISUCT => progressive_isuct(node),
+            Select::UCT(c) => uct(node, *c),
+            Select::ISUCT(c) => isuct(node, *c),
+            Select::ProgressiveUCT(c, d) => progressive_uct(node, *c, *d),
+            Select::ProgressiveISUCT(c, d) => progressive_isuct(node, *c, *d),
         }
     }
 }
 
-pub fn uct(node: &Node) -> f32 {
-    const C: f32 = 0.7;
-
+pub fn uct(node: &Node, c: f32) -> f32 {
     let stats = node.stats();
 
     let u = stats.reward / stats.visits as f32;
-    let v = C * ((node.parent_visits() as f32).ln() / stats.visits as f32).sqrt();
+    let v = c * ((node.parent_visits() as f32).ln() / stats.visits as f32).sqrt();
 
     u + v
 }
 
-pub fn isuct(node: &Node) -> f32 {
-    const C: f32 = 0.7;
-
+pub fn isuct(node: &Node, c: f32) -> f32 {
     let stats = node.stats();
 
     let u = stats.reward / stats.visits as f32;
-    let v = C * ((stats.availability as f32).ln() / stats.visits as f32).sqrt();
+    let v = c * ((stats.availability as f32).ln() / stats.visits as f32).sqrt();
 
     u + v
 }
 
-pub fn progressive_uct(node: &Node) -> f32 {
+pub fn progressive_uct(node: &Node, c: f32, d: f32) -> f32 {
     let stats = node.stats();
 
-    uct(node) + (-stats.value * 10.0) / stats.visits as f32
+    uct(node, c) + (-stats.value * d) / stats.visits as f32
 }
 
-pub fn progressive_isuct(node: &Node) -> f32 {
+pub fn progressive_isuct(node: &Node, c: f32, d: f32) -> f32 {
     let stats = node.stats();
 
-    isuct(node) + (-stats.value * 10.0) / stats.visits as f32
+    isuct(node, c) + (-stats.value * d) / stats.visits as f32
 }
